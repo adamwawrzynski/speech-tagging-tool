f9fb9057b6855704465efb26a09b2d2335d953bb
Adam Wawrzynski <adamwawrzynski@wp.pl>
2019-03-30 15:04:54 +0100

    Change module to manipulate audio signals, add main script
    Signed-off-by: Adam Wawrzynski <adamwawrzynski@wp.pl>

---
+++ modules/audio_processing.py
@@ -0,0 +1,24 @@
+from python_speech_features import mfcc
+from python_speech_features import logfbank
+from python_speech_features import delta
+import scipy.io.wavfile as wav
+import sox
+
+def get_features(source, destination, n_delta=1, numcep=13):
+    '''' Transform original sound file and calculate features. '''
+    # transform original file
+    tfm = sox.Transformer()
+    tfm.set_output_format(rate=16000)
+    tfm.build(source, destination)
+
+    # calulate features
+    rate,sig = wav.read(destination)
+    mfcc_feat = mfcc(sig, rate)
+    fbank_feat = logfbank(sig, rate, numcep=numcep)
+    if n_delta >= 1:
+        d_mfcc_feat = delta(mfcc_feat, 2)
+    if n_delta == 2:
+        d_mfcc_feat2 = delta(d_mfcc_feat, 2)
+        d_mfcc_feat = np.concatenate((d_mfcc_feat, d_mfcc_feat2), axis=1)
+    complete_mfcc = np.concatenate((mfcc_feat, d_mfcc_feat), axis=1)
+    return complete_mfcc
