4b9ca25cfb1cafe1a4319ffc4548f375088d4f2c
Adam Wawrzynski <adamwawrzynski@wp.pl>
2019-04-07 15:43:24 +0200

    Complete preprocessing
    Signed-off-by: Adam Wawrzynski <adamwawrzynski@wp.pl>

--- modules/audio_processing.py
+++ modules/audio_processing.py
@@ -4,22 +4,156 @@ from python_speech_features import delta
 import scipy.io.wavfile as wav
 import sox
 import numpy as np
+import os
 
-def get_features(source, destination, n_delta=1, numcep=13):
-    '''' Transform original sound file and calculate features. '''
+
+class Sample(object):
+    ''' Class representing sound signal with tagged phonemes. '''
+    def __init__(self):
+        self.features = None
+        self.phonemes = None
+
+    def __str__(self):
+        print("Features: {}".format(self.features))
+        print("Phonemes: {}".format(self.phonemes))
+
+    def set_features(self, features):
+        self.features = features
+
+    def set_phonemes(self, phonemes):
+        self.phonemes = phonemes
+
+
+def get_feasible_phonemes(path):
+    ''' Get feasible phones from file. '''
+    phonemes = {}
+    if(os.path.isfile(path)):
+        file = open(path)
+        counter=0
+        for line in file:
+            val = line[:-1]
+            phonemes[val] = counter
+            counter = counter + 1
+    return phonemes
+
+
+def get_phonemes_from_file(path):
+    ''' Parse .PHN file and return array of tagged phonemes. '''
+    file = open(path)
+    values = np.empty((3))
+    for line in file:
+        val = line.split()
+        values = np.append(values, np.asarray(val), axis=0)
+    values = values.reshape(int(values.shape[0]/3),3)
+    values = values[1:]
+    return values
+
+
+def calculate_features(source, destination, n_delta=1, numcep=13, winfunc=np.hamming):
+    '''' Transform sound file and calculate MFCC features. '''
     # transform original file
     tfm = sox.Transformer()
     tfm.set_output_format(rate=16000)
     tfm.build(source, destination)
-
+    
     # calulate features
     rate,sig = wav.read(destination)
-    mfcc_feat = mfcc(sig, rate, numcep=numcep)
-    fbank_feat = logfbank(sig, rate)
+    mfcc_feat = mfcc(sig, rate, numcep=numcep, winfunc=winfunc)
+
     if n_delta >= 1:
         d_mfcc_feat = delta(mfcc_feat, 2)
+        
     if n_delta == 2:
         d_mfcc_feat2 = delta(d_mfcc_feat, 2)
         d_mfcc_feat = np.concatenate((d_mfcc_feat, d_mfcc_feat2), axis=1)
+        
     complete_mfcc = np.concatenate((mfcc_feat, d_mfcc_feat), axis=1)
     return complete_mfcc
+
+
+def convert_phonemes_to_number(dataset, phonemes):
+    ''' Replace phonemes with correspoding number from feasible phonemes dictionary. '''
+    for i in dataset:
+            i[2] = phonemes[i[2]]
+    return dataset
+
+
+def get_framing_phonemes(dataset, features):
+    ''' Calculate phoneme of each sound signal frame. '''
+    tmp_dataset = []
+    pointer = 0
+    for j in dataset:
+        stop = j[1].astype(np.int)
+        for k in range(pointer, stop, 160):
+            tmp_dataset.append(j[2])
+            pointer = pointer + 160
+    return tmp_dataset
+
+
+def get_samples(path, feasible_phonemes):
+    ''' Extract dataset from path. '''
+    dataset = np.empty(1)
+
+    # extract basename of files and remove duplicates
+    filelist = os.listdir(path)
+    for i in range(0, len(filelist)):
+        filelist[i] = os.path.splitext(os.path.basename(filelist[i]))[0]
+    filelist = list(dict.fromkeys(filelist))
+
+    # process files
+    for filename in filelist:
+        sample = Sample()
+
+        # get list of directories
+        files = os.listdir(path)
+        files.sort()
+
+        # if path points to directory do recursive call
+        if(os.path.isdir(path + '/' + filename)):
+            sample = extract_dataset(path + '/' + filename, feasible_phonemes)
+
+        sample_name = "sample.wav"
+        # otherwise process files inside directory
+        else:
+            features = calculate_features(path + '/' + filename + ".WAV", sample_name)
+            os.remove(sample_name)
+            sample.set_features(features)
+            tmp = get_phonemes_from_file(path + '/' + filename + ".PHN")
+
+            # convert phoneme from ASCII to number representing class
+            tmp = convert_phonemes_to_number(tmp, feasible_phonemes)
+
+            # calculate frames of phonemes
+            phonemes = np.asarray(get_framing_phonemes(tmp, sample.features), dtype=int)
+
+            # remove excessive phoneme
+            phonemes = phonemes[:features.shape[0]]
+
+            # create array of one hot vectors respresenting phoneme
+            one_hot_vector = np.zeros((phonemes.shape[0], 61), dtype=int)
+            for i in range(0, len(phonemes)-1):
+                one_hot_vector[i] = np.eye(61)[phonemes[i]]
+
+            sample.set_phonemes(one_hot_vector)
+
+        # add processed sample to dataset
+        dataset = np.append(dataset, sample)
+
+    # remove excessive first row
+    dataset = dataset[1:]
+    return dataset
+
+
+def get_dataset(feasible_phonemes_path, dataset_path):
+    ''' Remove samples where audio duration don't match transcription duration. '''
+    phonemes = get_feasible_phonemes(feasible_phonemes_path)
+
+    dataset = get_samples(dataset_path, phonemes)
+
+    new_dataset = np.empty(1)
+    for i in range(0, len(dataset)):
+            if dataset[i].features.shape[0] == dataset[i].phonemes.shape[0]:
+                    new_dataset = np.append(new_dataset, dataset[i])
+
+    dataset = new_dataset[1:]
+    return dataset
