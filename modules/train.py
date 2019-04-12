67300cde94f24cdd9f0fa14216c06d669ee7c5bc
Adam Wawrzynski <adamwawrzynski@wp.pl>
2019-04-12 07:59:59 +0200

    Extract train model function
    Signed-off-by: Adam Wawrzynski <adamwawrzynski@wp.pl>

--- modules/train.py
+++ modules/train.py
@@ -1,43 +1,12 @@
 # -*- coding: utf-8 -*-
 import audio_processing as ap
+import os
 import models as md
 import numpy as np
 from keras.models import load_model
 from time import time
 from keras.callbacks import TensorBoard
 from keras import backend as K
-
-
-def train_model(model, name, dataset, epochs=25, train_test_ratio=0.1, restore=False):
-        tensorboard = TensorBoard(log_dir="logs/" + name)
-
-        # split dataset to training and testing
-        train_dataset = dataset[int(len(dataset)*train_test_ratio):]
-        test_dataset = dataset[0:int(len(dataset)*train_test_ratio)]
-
-        # get one sample of test dataset to validate model
-        X_test = test_dataset[0].features
-        X_test = X_test.reshape(1, X_test.shape[0], X_test.shape[1])
-        y_test = test_dataset[0].phonemes
-        y_test = y_test.reshape(1, y_test.shape[0], y_test.shape[1])
-
-        # load model to retrain
-        if restore == True:
-                model = load_model(name + ".hd5")
-                print("Model loaded from disk")
-
-        for i in range(0, epochs):
-                for k in train_dataset:
-                        x = k.features
-                        x = x.reshape(1, x.shape[0], x.shape[1])
-                        y = k.phonemes
-                        y = y.reshape(1, y.shape[0], y.shape[1])
-                        model.fit(x, y, batch_size=1, epochs=1,
-                                        callbacks=[tensorboard],
-                                        validation_data=(X_test, y_test))
-
-        model.save(name + ".hd5")
-        print("Model saved to disk")
 
 
 def decode_batch(result):
@@ -60,16 +29,28 @@ def evaluate_predictions(y_true, y_pred):
         return counter
 
 
-if __name__ == "__main__":
+def train_model(name,
+                epochs,
+                alphabet_path,
+                dataset_path,
+                restore,
+                tensorboard=False,
+                verbose=False):
+
+        weights_filename = name + ".hd5"
+
         # load alphabet and dataset from given paths
-        dataset = ap.get_dataset("../data/phonemes.txt",
-                                        "/home/adam/Downloads/TIMIT/TRAIN")
+        dataset = ap.get_dataset(alphabet_path, dataset_path)
 
         # load model
         model, test_func = md.custom_ctc_lstm()
 
         # run tensorboard logger
-        tensorboard = TensorBoard(log_dir="logs/" + "custom_ctc_lstm")
+        tb = TensorBoard(log_dir="logs/" + name)
+
+        callbacks = None
+        if tensorboard == True:
+                callbacks = [tb]
 
         # split dataset to training and testing
         train_test_ratio=0.1
@@ -90,7 +71,13 @@ if __name__ == "__main__":
         input_length = np.ones((1, 1))
         label_length = np.ones((1, 1))
 
-        epochs = 50
+        # load model to retrain
+        if restore == True:
+                if os.path.isfile(weights_filename):
+                        model.load_weights(weights_filename)
+                        print("Model weights loaded from disk")
+                else:
+                        print("Model weights not found")
 
         for i in range(0, epochs):
 
@@ -116,7 +103,7 @@ if __name__ == "__main__":
                                         y=np.zeros(x.shape[0]),
                                         batch_size=1,
                                         epochs=1,
-                                        callbacks=[tensorboard])
+                                        callbacks=callbacks)
 
                 # predict sequence
                 # it returns matrix of occurance probability for each phoneme
@@ -127,7 +114,19 @@ if __name__ == "__main__":
                 out = np.asarray(out)
 
                 # print results
-                #print("Predicted: \t{}".format(out))
-                #print("Actual: \t{}".format(y_test))
+                if verbose == True:
+                        print("Predicted: \t{}".format(out))
+                        print("Actual: \t{}".format(y_test))
+
                 wrong_predictions = evaluate_predictions(y_test, out)
                 print("Wrong predictions: {}/{}".format(wrong_predictions, len(out)))
+
+        model.save_weights(weights_filename)
+        print("Model weights saved to disk")
+
+if __name__ == "__main__":
+        train_model(name="custom_ctc_lstm",
+                        epochs=5,
+                        alphabet_path="../data/phonemes.txt",
+                        dataset_path="/home/adam/Downloads/TIMIT/TRAIN/DR1/FCJF0",
+                        restore=True)
