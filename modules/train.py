Remove obsolete variable, fix frame width in transcription calculation
Signed-off-by: Adam Wawrzynski <adamwawrzynski@wp.pl>

modules/train.py
@@ -238,8 +238,9 @@ def predict_model(name,
         result = decode_batch(result[0])
         result = np.asarray(result, dtype=int)
 
+        print(audio.shape)
         result = ap.convert_number_to_phoneme(result, phonemes)
-        create_transcription(result, transcription_path, window_width=20, verbose=verbose)
+        create_transcription(result, transcription_path, window_width=int(frame_width*1000), verbose=verbose)
 
 
 if __name__ == "__main__":