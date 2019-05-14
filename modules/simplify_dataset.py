4d0351c2a24a936cda3597bdeec32e9aa478b43e
Adam Wawrzynski <adamwawrzynski@wp.pl>
2019-05-14 20:50:03 +0200

    simplify_dataset.py: Add function to simplify feasible phonemes
    Signed-off-by: Adam Wawrzynski <adamwawrzynski@wp.pl>

--- modules/simplify_dataset.py
+++ modules/simplify_dataset.py
@@ -61,6 +61,45 @@ def process_file(filename):
     os.remove(filename)
     os.rename(filename+"tmp", filename)
 
+
+def simplify_phonemes_file(filename):
+    ''' Simplify phonemes.txt file from 61 to 39 phonemes. '''
+    with open(filename, "rt") as fin:
+        with open(filename+"tmp", "wt") as fout:
+                for line in fin:
+                    if 'ao' in line or \
+                        'ax' in line or \
+                        'ax-h' in line or \
+                        'ah-h' in line or \
+                         'ahr' in line or \
+                         'axr' in line or \
+                         'hv' in line or \
+                         'ix' in line or \
+                         'el' in line or \
+                         'em' in line or \
+                         'en' in line or \
+                         'nx' in line or \
+                         'eng' in line or \
+                         'zh' in line or \
+                         'ux' in line or \
+                         'q' in line or \
+                         'pcl' in line or \
+                         'tcl' in line or \
+                         'kcl' in line or \
+                         'bcl' in line or \
+                         'dcl' in line or \
+                         'gcl' in line or \
+                         'pau' in line or \
+                        'epi' in line:
+                        pass
+                    elif 'h#' in line:
+                        fout.write('sil\n')
+                    else:
+                        fout.write(line)
+    os.remove(filename)
+    os.rename(filename+"tmp", filename)
+
+
 def simplify_dataset(path):
     ''' Simplify dataset from 61 to 39 phonemes. '''
     # extract basename of files and remove duplicates
@@ -86,3 +125,4 @@ def simplify_dataset(path):
 
 if __name__ == "__main__":
     simplify_dataset(sys.argv[1])
+    simplify_phonemes_file(sys.argv[1])
